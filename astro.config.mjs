b9d8dfa7cb966b4ed6c39b89ea295da502bee560
Matrox1821 <matiasgg1821@gmail.com>
2024-10-27 19:35:37 -0300

    fix config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercelServerless from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercelServerless(),
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
