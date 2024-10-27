614cef970cf3ce2a7c148539c4d431cc6170b565
Matrox1821 <matiasgg1821@gmail.com>
2024-10-27 19:23:46 -0300

    change vercel configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel(),
+  adapter: vercelServerless(),
 });
