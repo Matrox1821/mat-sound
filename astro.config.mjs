6cc2e5f8c409d3ff1745753cd8b32237f03c851f
Matrox1821 <matiasgg1821@gmail.com>
2024-10-27 20:14:19 -0300

    fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel({
-    edgeMiddleware: true,
-  }),
+  adapter: vercel(),
 });
