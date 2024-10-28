400ba0bb546b694df85655bd419acd9f6b333683
Matrox1821 <matiasgg1821@gmail.com>
2024-10-27 21:26:11 -0300

    netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), react()],
-  adapter: vercel(),
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
 });
