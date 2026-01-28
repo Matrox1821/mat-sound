66ad5e9d2b57ce8be1a8e40bdb1f4852faa4f083
Matias <matiasgg1821@gmail.com>
2026-01-28 01:06:59 -0300

    fix error

--- src/components/ui/buttons/index.tsx
+++
@@ -1,5 +0,0 @@
-export { FollowButton } from "./FollowButton";
-export { CarouselCardPlayButton } from "./CarouselCardPlayButton";
-export { LikeButton } from "./LikeButton";
-export { SaveInPlaylist } from "./SaveInPlaylist";
-export { SwiperButtons } from "./SwiperButtons";
