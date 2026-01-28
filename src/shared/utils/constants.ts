e103f7dd74ac201904f5913ca0b96c1cb54d14f7
Matias <matiasgg1821@gmail.com>
2026-01-28 02:03:58 -0300

    update url

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -1,7 +1,5 @@
 const GET_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://learn-languages-zeta.vercel.app"
-    : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? "https://mat-sound.vercel.app/" : "http://localhost:3000";
 
 const MAX_SIZE_MOBILE = 768;
 
