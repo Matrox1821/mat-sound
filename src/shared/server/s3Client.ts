164b55f64b0a9913d1fb96e0d041b0dcb36bb71e
Matias <matiasgg1821@gmail.com>
2026-01-31 20:52:46 -0300

    debug: push for debug data

--- src/shared/server/s3Client.ts
+++ src/shared/server/s3Client.ts
@@ -1,11 +1,25 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
+const endpoint = process.env.CLOUDFLARE_ENDPOINT;
+const accessKeyId = process.env.CLOUDFLARE_ACCESS_KEY;
+const secretAccessKey = process.env.CLOUDFLARE_SECRET_ACCESS_KEY;
+
+if (!endpoint) {
+  throw new Error("Faltan variables de entorno de Cloudflare (ENDPOINT)");
+}
+if (!accessKeyId) {
+  throw new Error("Faltan variables de entorno de Cloudflare (ACCESS_KEY)");
+}
+if (!secretAccessKey) {
+  throw new Error("Faltan variables de entorno de Cloudflare (SECRET_KEY)");
+}
+
 const s3Client = new S3Client({
   region: "auto",
-  endpoint: process.env.CLOUDFLARE_ENDPOINT || "",
+  endpoint: endpoint || "",
   credentials: {
-    accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY || "",
-    secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY || "",
+    accessKeyId: accessKeyId || "",
+    secretAccessKey: secretAccessKey || "",
   },
 });
 
