0d154e6481eee6ed145a42d3c30732c99eda7a28
Matias <matiasgg1821@gmail.com>
2026-04-25 18:03:15 -0300

    fix: type errors

--- src/components/media/preloadTrack/index.tsx
+++ src/components/media/preloadTrack/index.tsx
@@ -1,9 +1,10 @@
-import { ContentTrack } from "@shared-types/content.types";
+import { MediaCard } from "@/types/content.types";
 
 interface CarouselCardPlayButtonProps {
-  track: ContentTrack;
+  track: MediaCard;
 }
 
 export const PreloadTrack = ({ track }: CarouselCardPlayButtonProps) => {
+  if (track.type !== "tracks" || !track.song) return;
   return <link rel="preload" as="fetch" href={track.song} crossOrigin="anonymous" />;
 };
