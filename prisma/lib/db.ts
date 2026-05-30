75f8adc217650096856efe81f8a29c9a38757606
Matias <matiasgg1821@gmail.com>
2026-05-30 14:49:33 -0300

    change prisma client fetch

--- prisma/lib/db.ts
+++ prisma/lib/db.ts
@@ -1,17 +1,7 @@
 import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof prismaClientSingleton> | undefined;
-};
-
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    accelerateUrl: process.env.DATABASE_PRISMA_DATABASE_URL ?? "",
-  }).$extends(withAccelerate());
-};
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+export { prisma };
